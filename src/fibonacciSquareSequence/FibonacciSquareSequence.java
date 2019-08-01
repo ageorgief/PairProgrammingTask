44c9165dbb7fe8924f670be99177d0b53a859511
Andrey Georgiev <andreyg673@gmail.com>
2019-08-01 15:09:38 +0300

    Comitting something

--- src/fibonacciSquareSequence/FibonacciSquareSequence.java
+++ src/fibonacciSquareSequence/FibonacciSquareSequence.java
@@ -11,28 +11,53 @@ public class FibonacciSquareSequence {
 		return (currentNumber == number2 && previousNumber == number1);
 	}
 
-	public static boolean spiralMatrixWalk(int size, int firstCoordinate, int secondCoordinate) {
+	public static boolean spiralMatrixWalk(int size, int firstCoordinate, int secondCoordinate, int[][] matrix) {
 		int previousNumber = matrix[firstCoordinate][secondCoordinate];
-		if (isFibonnaciNumber(matrix[firstCoordinate][secondCoordinate], secondCoordinate)) {
-			for (int i = 0; i < size / 2 + 1; i++) {
-				for (int j = i; j < size - i; j++) {
-					if(i ==0 && j > 1 || i > 0) {
-						if()
+		int currentNumber = matrix[firstCoordinate][secondCoordinate + 1];
+		int counter = 0;
+		if (isFibonnaciNumber(previousNumber, currentNumber)) {
+			for (int i = firstCoordinate; i < size / 2 + 1; i++) {
+				for (int j = secondCoordinate; j < size - i; j++) {
+					counter++;
+					if (counter > 2) {
+						if (matrix[i][j] != previousNumber + currentNumber) {
+							return false;
+						} else {
+							previousNumber = currentNumber;
+							currentNumber = matrix[i][j];
+						}
 					}
 				}
 				for (int j = i + 1; j < size - i; j++) {
+					if (matrix[j][size - i - 1] != previousNumber + currentNumber) {
+						return false;
+					} else {
+						previousNumber = currentNumber;
+						currentNumber = matrix[j][size - i - 1];
+					}
 				}
 				for (int j = size - i - 2; j >= 0 + i; j--) {
+					if (matrix[size - i - 1][j] != previousNumber + currentNumber) {
+						return false;
+					} else {
+						previousNumber = currentNumber;
+						currentNumber = matrix[size - i - 1][j];
+					}
 				}
 				for (int j = size - i - 2; j > 0 + i; j--) {
+					if (matrix[j][i] != previousNumber + currentNumber) {
+						return false;
+					} else {
+						previousNumber = currentNumber;
+						currentNumber = matrix[j][i];
+					}
 				}
 			}
-		}
-		else {
+		} else {
 			return false;
 		}
 
-		return false;
+		return true;
 	}
 
 	public static boolean containsFibonacciSequence(int[][] matrix) {
--- src/fibonacciSquareSequence/FibonacciSquareSequenceTest.java
+++ src/fibonacciSquareSequence/FibonacciSquareSequenceTest.java
@@ -11,5 +11,8 @@ public class FibonacciSquareSequenceTest {
 		assertTrue(FibonacciSquareSequence.isFibonnaciNumber(317811,514229));
 		assertFalse(FibonacciSquareSequence.isFibonnaciNumber(22,22));
 	}
+	public void testSpiralMatrixWalk() {
+		
+	}
 
 }
